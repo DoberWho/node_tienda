8fb114c4c1f5eeffdd31eb751bad6a4f7da4be0a
Jorge Parada Fernandez <jorge.parada@ctbarbanza.com>
2021-02-03 12:58:37 +0100

    CORS: Ver las Cabeceras y permitir Origines Externos

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 
 let app = express()
 const port = process.env.PORT || 3000
@@ -10,6 +10,30 @@ const MONGODB = 'mongodb://localhost:27017/tienda'
 mongoose.connect(MONGODB, { useNewUrlParser: true })
 .then(()=> { console.log('OK: '+MONGODB)})
 .catch(()=> { console.log('Error: '+MONGODB)}) 
+
+
+corsOpts = function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Accept,X-ACCESS_TOKEN,Authorization,authorization,x-access-token');
+    res.header("Access-Control-Allow-Headers", "'User-Agent','sec-ch-ua-mobile','sec-ch-ua','Referer','Accept','Content-Length', Origin, X-Requested-With, Content-Type, Accept,X-ACCESS_TOKEN,Authorization,authorization,x-access-token");
+    res.header("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
+    //res.setHeader('Access-Control-Allow-Credentials', true);
+    if ('OPTIONS' === req.method) {
+        //respond with 200
+        console.log("REQ", "OPTIONS");
+        res.sendStatus(200);
+      }
+      else {
+      //move on
+        next();
+      }
+  }
+  
+  app.use(corsOpts);
+  app.all('*', corsOpts);
+  app.use(cors({
+    exposedHeaders: ['User-Agent','sec-ch-ua-mobile','sec-ch-ua','Referer','Accept','Content-Length', 'X-Foo', 'X-Bar', 'OPTIONS', 'Origin', 'Content-Type', 'X-Requested-With', 'X-ACCESS_TOKEN', 'Authorization', 'authorization', 'x-access-token'],
+  }));
 
 app.listen(port) 
 app.use(bodyParser.json({limit: '50mb'}));
