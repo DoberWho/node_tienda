b9bbfc14f2ecd646a39e6e618d30f0893b7a089f
Jorge Parada Fernandez <jorge.parada@ctbarbanza.com>
2021-02-12 12:51:54 +0100

    ✨ ADD: MiddleWare => isAdmin

--- categoria/categoria.route.js
+++ categoria/categoria.route.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router  = express.Router()
+const m = require('../middleware')
 
 const ctrl = require('./categoria.controller')
 
-router.get('/',  ctrl.get)
-router.post('/', ctrl.post) 
-router.put('/', ctrl.editar) 
-router.get('/:id', ctrl.detail) 
-router.delete('/:id', ctrl.delete)  
+router.get('/',                    ctrl.get)
+router.post('/',      m.isAdmin,   ctrl.post) 
+router.put('/',       m.isAdmin,   ctrl.editar) 
+router.get('/:id',                 ctrl.detail) 
+router.delete('/:id', m.isAdmin,   ctrl.delete)  
 
 module.exports = router;
--- middleware.js
+++ middleware.js
@@ -41,3 +41,49 @@ exports.isLoged = async function (req, res, next) {
     jwt.verify(token, 'HolaMundo.1', testToken) 
   
 }
+
+exports.isAdmin = async function (req, res, next) {
+
+    const body = req.body;
+    const query = req.query;
+    const params = req.params;  
+
+    let headers = req.headers
+    console.log(headers)
+    
+    let token = headers.authorization 
+
+    let testToken = async function(error, decoded) {
+        if (error){
+            let code = 401
+            return res.status(code).json(error)
+        }
+
+        let id = decoded._id
+        let user
+        try {
+            user = await model.findById(id)
+        } catch (error) {
+            let code = 404
+            return res.status(code).json("USER_NOT_FOUND")
+        }
+        if (!user){
+            let code = 404
+            return res.status(code).json("USER_NOT_FOUND")
+        }
+
+        if (user.role != 'admin'){
+            let code = 403
+            return res.status(code).json("USER_NOT_ALLOWED")
+        }
+    
+        req.usuario = user
+        
+        next()
+
+    }
+
+
+    jwt.verify(token, 'HolaMundo.1', testToken) 
+  
+}
